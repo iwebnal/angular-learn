edfb8e50c2b2271ba2f284eb408772613b0984a2
iwebnal <iwebnal@gmail.com>
2019-04-10 16:14:35 +0300

    [+] Learn data-binding for angular templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'Parent component:';
+  
+  public title:string = 'Parent component:';
+  public myClass:string = 'red';
+
+  constructor() {
+
+    setTimeout(() => {
+      this.myClass = 'blue';
+
+      setTimeout(() => {
+        this.myClass = 'green';
+      }, 2000)
+    
+    }, 2000)
+
+  }
+
+
+
 }
