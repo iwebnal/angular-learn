e97591f214e0f75710e2c61fdce73e5f6731123d
iwebnal <iwebnal@gmail.com>
2019-04-21 22:50:06 +0300

    [*] Angular *ngFor directive

--- src/app/header/user-card/user-card.component.ts
+++ src/app/header/user-card/user-card.component.ts
@@ -10,6 +10,14 @@ export class UserCardComponent implements OnInit {
   public isShow: boolean = true;
   public butttonText: string = 'Show text';
 
+  public people = [
+    {name: 'John', age: 24},
+    {name: 'Tom', age: 27},
+    {name: 'Ann', age: 28},
+    {name: 'Edd', age: 31},
+    {name: 'Jony', age: 30},
+  ]
+
   @Input('colorName') colorName: string = '';
   @Input('cardName') cardName: any; // 'cardName' - atribute name
 
