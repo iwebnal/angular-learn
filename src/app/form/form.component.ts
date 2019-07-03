fd8f5b102ef54871f43cc5864a1e80daf69dc6c5
iwebnal <iwebnal@gmail.com>
2019-07-03 11:31:58 +0300

    [*] Working with FormGroup and FormArray

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 
 @Component({
@@ -10,6 +10,8 @@ import { Observable, of } from 'rxjs';
 export class FormComponent implements OnInit {
 
   public nameControl: FormControl;
+  public fullnameControl: FormGroup;
+  public userListControl: FormGroup;
 
   constructor() { }
 
@@ -23,7 +25,35 @@ export class FormComponent implements OnInit {
       console.log(this.nameControl.errors);
       console.log(status)
     });
+
+    this.fullnameControl = new FormGroup({
+      firstName: new FormControl,
+      lastName: new FormControl
+    });
+
+    this.fullnameControl.valueChanges.subscribe((value) => console.log(value));
+
+    this.userListControl = new FormGroup({
+      users: new FormArray([
+        new FormControl('Alice'),
+        new FormControl('Bob'),
+        new FormControl('John')
+      ])
+    });
+
+    this.userListControl.valueChanges.subscribe((value) => console.log(value));
+
   }
+
+  public removeUserControl(index) {
+    (this.userListControl.controls['users'] as FormArray).removeAt(index);
+  }
+
+  public addUserControl() {
+    (this.userListControl.controls['users'] as FormArray).push(new FormControl(''));
+  }
+
+
 
   myValidator(formControl:FormControl) {
     if(formControl.value.length < 3) {
